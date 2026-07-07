4580ad8434db5a507e3e75cfeadeaeb50025163a
felipemenezes098 <felipemenezes.98@outlook.com>
2026-07-07 19:20:46 -0300

    feat(logo): add BaseUI logo component

--- src/components/core/logo.tsx
+++ src/components/core/logo.tsx
@@ -178,4 +178,16 @@ export const Logo = {
       />
     </svg>
   ),
+  BaseUI: (props: React.SVGProps<SVGSVGElement>) => (
+    <svg width="17" height="24" viewBox="0 0 17 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M9.5001 7.01537C9.2245 6.99837 9 7.22385 9 7.49999V23C13.4183 23 17 19.4183 17 15C17 10.7497 13.6854 7.27351 9.5001 7.01537Z"
+      ></path>
+      <path
+        fill="currentColor"
+        d="M8 9.8V12V23C3.58172 23 0 19.0601 0 14.2V12V1C4.41828 1 8 4.93989 8 9.8Z"
+      ></path>
+    </svg>
+  ),
 }
